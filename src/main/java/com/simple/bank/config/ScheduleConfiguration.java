b12b112302d4868a9f6e2840cfa0a6697b84bd68
ccfluo <chuanfenluo@163.com>
2025-08-25 09:56:04 +0800

    Version 0.4 : Add transaction summary report at 2am daily

--- src/main/java/com/simple/bank/config/ScheduleConfiguration.java
+++ src/main/java/com/simple/bank/config/ScheduleConfiguration.java
@@ -2,14 +2,57 @@ package com.simple.bank.config;
 
 //import com.simple.bank.job.DemoJob01;
 //import com.simple.bank.job.DemoJob02;
+import com.simple.bank.job.JobEnd;
+import com.simple.bank.job.JobStart;
 import com.simple.bank.job.TransactionStatisticsJob;
 import org.quartz.*;
+import org.quartz.listeners.JobChainingJobListener;
+import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ScheduleConfiguration {
 
+
+    @Configuration
+    public static class JobStartConfiguration {
+
+        @Bean
+        public JobDetail jobStart() {
+            return JobBuilder.newJob(JobStart.class)
+                    .withIdentity("jobStart")
+                    .storeDurably()
+                    .build();
+        }
+
+
+        @Bean
+        public Trigger jobStartTrigger() {
+            // 基于 Quartz Cron 表达式的调度计划的构造器
+            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.dailyAtHourAndMinute(2, 0);
+            // Trigger 构造器
+            return TriggerBuilder.newTrigger()
+                    .forJob(jobStart()) // 对应 Job 为 transactionStatisticsJob
+                    .withIdentity("jobStartTrigger") // 名字为 jobStartTrigger
+                    .withSchedule(scheduleBuilder) // 对应 Schedule 为 scheduleBuilder
+                    .build();
+        }
+    }
+
+    @Configuration
+    public static class JobEndConfiguration {
+
+        @Bean
+        public JobDetail jobEnd() {
+            return JobBuilder.newJob(JobEnd.class)
+                    .withIdentity("jobEnd")
+                    .storeDurably()
+                    .build();
+        }
+    }
+
+    @Configuration
     public static class transactionStatisticsJobConfiguration {
 
         @Bean
@@ -19,18 +62,44 @@ public class ScheduleConfiguration {
                     .storeDurably() // 没有 Trigger 关联的时候任务是否被保留。因为创建 JobDetail 时，还没 Trigger 指向它，所以需要设置为 true ，表示保留。
                     .build();
         }
+//
+//        @Bean
+//        public Trigger transactionStatisticsJobTrigger() {
+//            // 基于 Quartz Cron 表达式的调度计划的构造器
+//            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.dailyAtHourAndMinute(9, 37);
+//            // Trigger 构造器
+//            return TriggerBuilder.newTrigger()
+//                    .forJob(transactionStatisticsJob()) // 对应 Job 为 transactionStatisticsJob
+//                    .withIdentity("transactionStatisticsJobTrigger") // 名字为 transactionStatisticsJobTrigger
+//                    .withSchedule(scheduleBuilder) // 对应 Schedule 为 scheduleBuilder
+//                    .build();
+//        }
+    }
 
+    @Configuration
+    public static class JobChainConfiguration {
+        /**
+         * Job dependency：TransactionStatisticsJob  → JobEnd
+         */
         @Bean
-        public Trigger transactionStatisticsJobTrigger() {
-            // 基于 Quartz Cron 表达式的调度计划的构造器
-            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.dailyAtHourAndMinute(2, 0);
-            // Trigger 构造器
-            return TriggerBuilder.newTrigger()
-                    .forJob(transactionStatisticsJob()) // 对应 Job 为 demoJob02
-                    .withIdentity("transactionStatisticsJobTrigger") // 名字为 demoJob02Trigger
-                    .withSchedule(scheduleBuilder) // 对应 Schedule 为 scheduleBuilder
-                    .build();
+        public JobListener jobChainingListener(
+                JobDetail jobStart,
+                JobDetail transactionStatisticsJob,
+                JobDetail jobEnd) {
+            JobChainingJobListener listener = new JobChainingJobListener("chainListener");
+            listener.addJobChainLink(jobStart.getKey(), transactionStatisticsJob.getKey());
+            listener.addJobChainLink(transactionStatisticsJob.getKey(), jobEnd.getKey());
+            return listener;
+        }
+
+        /**
+         * listener register into Scheduler
+         */
+        @Bean
+        public SchedulerFactoryBeanCustomizer schedulerCustomizer(JobListener jobChainingListener) {
+            return schedulerFactoryBean -> schedulerFactoryBean.setGlobalJobListeners(jobChainingListener);
         }
 
     }
+
 }
---
+++ src/main/java/com/simple/bank/job/JobStart.java
@@ -0,0 +1,15 @@
+package com.simple.bank.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+@Slf4j
+public class JobStart extends QuartzJobBean {
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+        log.info("===== Start today's job execution =====");
+    }
+}
